139efd3be7e631d31d43c1f335171ca5c0a260d7
anekil <78557681+anekil@users.noreply.github.com>
2023-11-15 13:57:13 +0100

    database configuration

--- backend/website/models.py
+++ backend/website/models.py
@@ -5,9 +5,17 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(40), unique=True)
-    username = db.Column(db.String(20))
     password = db.Column(db.String(20))
+    ratings = db.relationship('Rating')
 
 
 class Rating(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('game.id'))
+    rating = db.Column(db.Boolean)
+
+
+class Game(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30))
