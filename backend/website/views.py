f005faa51422c4c3c8c65721a03dd6cbca1af01c
anekil <78557681+anekil@users.noreply.github.com>
2023-11-18 17:04:12 +0100

    added fetching game data

--- backend/website/views.py
+++ backend/website/views.py
@@ -1,3 +1,4 @@
+from requests import request
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
 
@@ -8,3 +9,17 @@ views = Blueprint('views', __name__)
 @login_required
 def home():
     return render_template("home.html", user=current_user)
+
+
+@views.route('/games')
+@login_required
+def games():
+    url = "https://api.igdb.com/v4/games"
+    payload = "fields name, cover.url; limit 50; where total_rating > 50 & category = 0; sort total_rating_count desc; "
+    headers = {
+        'Authorization': 'Bearer 7uasv2bw3iyqjavppma1c5yntc1geo',
+        'Client-ID': 't6vglpbbejgf6vm4ptt5q5lsrlros2',
+    }
+    response = request("POST", url, headers=headers, data=payload)
+    data = response.json()
+    return render_template("games.html", user=current_user, data=data)
